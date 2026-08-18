e586dc7520294b93cc8b466c2c20e63f75dfc297
Aniket Mullick <mullickaniket5@gmail.com>
2026-08-18 20:54:59 +0530

    Added the Evictor

--- Evictor.java
+++ Evictor.java
@@ -2,7 +2,26 @@ import java.util.concurrent.*;
 public class Evictor implements Runnable
 {
     BlockingQueue<SecretLease> DQ= new DelayQueue<SecretLease>();
+    ConcurrentMap<String, SecretLease> lmap=new ConcurrentHashMap<>();
+    public Evictor(LeaseManager lm)
+    {
+        DQ=lm.dq;
+        lmap=lm.leaseMap;
+    }
+    @Override
     public void run()
     {
+        try{
+            while(true)
+            {
+                SecretLease top=DQ.take();
+                lmap.remove(top.secretId());
+                top.secret().close();
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Evictor thread interrupted");
+        }
     }
 }
--- LeaseManager.java
+++ LeaseManager.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.util.concurrent.*;
 public class LeaseManager
 {
