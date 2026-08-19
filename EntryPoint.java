b3673440c58c7e5c96e8ed11684f231d33361ea2
Aniket Mullick <mullickaniket5@gmail.com>
2026-08-19 22:34:36 +0530

    Added the POST method

--- EntryPoint.java
+++ EntryPoint.java
@@ -4,6 +4,11 @@ import java.util.*;
 import java.net.*;
 public class EntryPoint
 {
+    static LeaseManager manager=new LeaseManager();
+    public EntryPoint(LeaseManager manager)
+    {
+        EntryPoint.manager=manager;
+    }
     public static void main(String[] args) throws IOException
     {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
@@ -16,6 +21,18 @@ public class EntryPoint
         @Override
         public void handle(HttpExchange request) throws IOException
         {
+            if("POST".equalsIgnoreCase(request.getRequestMethod()))
+            {
+                byte[] temp=request.getRequestBody().readAllBytes();
+                OffHeapSecret phantom=new OffHeapSecret(temp);
+                Arrays.fill(temp,(byte)0);
+                long time = Long.parseLong(request.getRequestHeaders().get("secret-ttl").get(0));
+                String ownerId=request.getRequestHeaders().get("owner-id").get(0);
+                String secretId=request.getRequestHeaders().get("secret-id").get(0);
+                SecretLease lease=new SecretLease(secretId,ownerId,time,phantom);
+                manager.store(lease);
+            }
+
             if("GET"==request.getRequestMethod())
             {
                 String q=request.getResponseHeaders().getFirst("Authorization");
